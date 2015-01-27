51dbc23708a3a0506c1e717e3212d3351cca662e
Peter Cooper <git@peterc.org>
2015-01-27 04:45:12 +0000

    Fix for Heroku

--- config/puma.rb
+++ config/puma.rb
@@ -1,8 +1,6 @@
 workers Integer(ENV['WEB_CONCURRENCY'] || 2)
 threads Integer(ENV['MIN_THREADS'] || 6), Integer(ENV['MAX_THREADS'] || 12)
 
-preload_app!
-
 rackup      DefaultRackup
 port        ENV['PORT']     || 5000
 environment ENV['RACK_ENV'] || 'development'
--- config/sequel.rb
+++ config/sequel.rb
@@ -1,8 +1,8 @@
 Sequel.extension(:pg_hstore, :pg_hstore_ops, :pg_array)
 
+DB.disconnect if defined?(DB)
 DB = Sequel.connect(ENV['DATABASE_URL'])
 DB.run("CREATE EXTENSION hstore") rescue nil
-
 # DB.loggers << Logger.new(STDOUT) if development?
 
 class Sequel::Model
